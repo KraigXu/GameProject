f01b777009eada6642ec6af7b189757a94d5c0f6
KraigXu <601724453@qq.com>
2019-03-12 10:22:48 +0800

    Update

--- TestProject/Assets/Antiquity World/Scripts/Skill/Behavior/ProjectionEffectTrigger.cs
+++ TestProject/Assets/Antiquity World/Scripts/Skill/Behavior/ProjectionEffectTrigger.cs
@@ -14,6 +14,7 @@ namespace GameSystem.Skill
     public class ProjectionEffectTrigger : SkillBehavior
     {
         public int ProjectileEffectId;
+        public int ProjectileNumber;
         public int EffectImpactId;
         public Transform Effecttf;
 
@@ -27,11 +28,24 @@ namespace GameSystem.Skill
         {
             if (curTime >= m_StartTime && m_IsExected == false)
             {
-                SkillData skillData = FightingScene.Instance.GetSkillData(ProjectileEffectId);
-                Effecttf = WXPoolManager.Pools[Define.PoolName].Spawn(skillData.Prefab, controller.transform, controller.transform.position, controller.transform.rotation);
+                if (Effecttf == null)
+                {
+                    SkillData skillData = FightingScene.Instance.GetSkillData(ProjectileEffectId);
+                    Vector3[] angles=new Vector3[ProjectileNumber];
 
-                WXProjectile projectile = Effecttf.gameObject.AddComponent<WXProjectile>();
-                projectile.EffectImpactId = EffectImpactId;
+                    float roation2 = ProjectileNumber * 15f/2f;
+                    for (int i = 0; i < angles.Length; i++)
+                    {
+                        Vector3 angle = controller.transform.eulerAngles+new Vector3(0,15*i-roation2,0);
+                        Effecttf = WXPoolManager.Pools[Define.PoolName].Spawn(skillData.Prefab, controller.transform.position + new Vector3(0, 1.5f, 0), controller.transform.rotation);
+                        Effecttf.Rotate(angle);
+
+                        WXProjectile projectile = Effecttf.GetComponent<WXProjectile>();
+                        projectile.EffectImpactId = EffectImpactId;
+                    }
+
+                }
+
                 m_IsExected = true;
                 return true;
             }
@@ -39,13 +53,19 @@ namespace GameSystem.Skill
             return false;
         }
 
+        /// <summary>
+        /// 0,0,15,3,16,0,3,360,10,0,0,3
+        /// </summary>
+        /// <param name="args"></param>
         public override void Init(string args)
         {
-            Debug.Log(args);
-            string[] values = args.Split(',');
+            string[] values = Define.SkillDataSplit(args);
+
             m_StartTime = float.Parse(values[1]);
             ProjectileEffectId = int.Parse(values[2]);
+            ProjectileNumber = int.Parse(values[3]);
             EffectImpactId = int.Parse(values[4]);
+            
         }
 
         public override void Reset()
