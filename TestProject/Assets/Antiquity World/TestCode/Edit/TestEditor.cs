b6a597ac2bb9db27c22c0c16bad0ee071bc08d51
KraigXu <601724453@qq.com>
2019-04-02 17:27:09 +0800

    Update

---
+++ TestProject/Assets/Antiquity World/TestCode/Edit/TestEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TestE))]
+public class TestEditor:Editor
+{
+
+    private TestE _test;
+
+    private void OnEnable()
+    {
+        _test=target as TestE;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label(_test.Value);
+        EditorGUILayout.EndHorizontal();
+
+    }
+
+    public void OnSceneGUI()
+    {
+        Handles.Label(_test.transform.position,"这是目标");
+    }
+}
