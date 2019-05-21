459e8923255a2429c43d720695ffbdaa9ab362aa
KraigXu <601724453@qq.com>
2019-05-21 17:27:51 +0800

    Update

---
+++ TestProject/Assets/Antiquity World/TestCode/Test3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test3 : MonoBehaviour
+{
+
+    public RectTransform node;
+
+	// Use this for initialization
+	void Start () {
+        //node.anchorMax=new Vector2(10,10);
+		node.offsetMax=new Vector2(80,75);
+	    node.offsetMin =new Vector2(0,-75);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
