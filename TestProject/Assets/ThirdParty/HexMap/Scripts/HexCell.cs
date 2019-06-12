37ee67cec180fbe639fd73d5d3adef5c78f742e1
KraigXu <601724453@qq.com>
2019-06-12 17:31:31 +0800

    Update

--- TestProject/Assets/ThirdParty/HexMap/Scripts/HexCell.cs
+++ TestProject/Assets/ThirdParty/HexMap/Scripts/HexCell.cs
@@ -9,16 +9,8 @@ public class HexCell : MonoBehaviour {
 	public RectTransform uiRect;
 
 	public HexGridChunk chunk;
-    public HexCell PathFrom;
-    public int SearchHeuristic { get; set; }
 
-    public int SearchPriority
-    {
-        get { return Distance + SearchHeuristic; }
-    }
-    public HexCell NextWithSamePriority { get; set; }
-
-    public int Elevation {
+	public int Elevation {
 		get {
 			return elevation;
 		}
@@ -229,9 +221,24 @@ public class HexCell : MonoBehaviour {
 		}
 		set {
 			distance = value;
-			UpdateDistanceLabel();
 		}
 	}
+
+	public HexUnit Unit { get; set; }
+
+	public HexCell PathFrom { get; set; }
+
+	public int SearchHeuristic { get; set; }
+
+	public int SearchPriority {
+		get {
+			return distance + SearchHeuristic;
+		}
+	}
+
+	public int SearchPhase { get; set; }
+
+	public HexCell NextWithSamePriority { get; set; }
 
 	int terrainTypeIndex;
 
@@ -414,11 +421,17 @@ public class HexCell : MonoBehaviour {
 					neighbor.chunk.Refresh();
 				}
 			}
+			if (Unit) {
+				Unit.ValidateLocation();
+			}
 		}
 	}
 
 	void RefreshSelfOnly () {
 		chunk.Refresh();
+		if (Unit) {
+			Unit.ValidateLocation();
+		}
 	}
 
 	public void Save (BinaryWriter writer) {
@@ -489,22 +502,19 @@ public class HexCell : MonoBehaviour {
 		}
 	}
 
-	void UpdateDistanceLabel () {
+	public void SetLabel (string text) {
 		UnityEngine.UI.Text label = uiRect.GetComponent<Text>();
-		label.text = distance == int.MaxValue ? "" : distance.ToString();
+		label.text = text;
 	}
 
+	public void DisableHighlight () {
+		Image highlight = uiRect.GetChild(0).GetComponent<Image>();
+		highlight.enabled = false;
+	}
 
-    public void DisableHighlight()
-    {
-        Image highlight = uiRect.GetChild(0).GetComponent<Image>();
-        highlight.enabled = false;
-    }
-
-    public void EnableHighlight(Color color)
-    {
-        Image highlight = uiRect.GetChild(0).GetComponent<Image>();
-        highlight.color = color;
-        highlight.enabled = true;
-    }
+	public void EnableHighlight (Color color) {
+		Image highlight = uiRect.GetChild(0).GetComponent<Image>();
+		highlight.color = color;
+		highlight.enabled = true;
+	}
 }
--- TestProject/Assets/ThirdParty/HexMap/Scripts/HexCellPriorityQueue.cs
+++ TestProject/Assets/ThirdParty/HexMap/Scripts/HexCellPriorityQueue.cs
@@ -1,81 +1,63 @@
 ﻿using System.Collections.Generic;
 
-public class HexCellPriorityQueue
-{
+public class HexCellPriorityQueue {
 
-    List<HexCell> list = new List<HexCell>();
+	List<HexCell> list = new List<HexCell>();
 
-    int count=0;
+	int count = 0;
+	int minimum = int.MaxValue;
 
-    int minimum=int.MaxValue;
+	public int Count {
+		get {
+			return count;
+		}
+	}
 
+	public void Enqueue (HexCell cell) {
+		count += 1;
+		int priority = cell.SearchPriority;
+		if (priority < minimum) {
+			minimum = priority;
+		}
+		while (priority >= list.Count) {
+			list.Add(null);
+		}
+		cell.NextWithSamePriority = list[priority];
+		list[priority] = cell;
+	}
 
-    public int Count
-    {
-        get { return count; }
-    }
+	public HexCell Dequeue () {
+		count -= 1;
+		for (; minimum < list.Count; minimum++) {
+			HexCell cell = list[minimum];
+			if (cell != null) {
+				list[minimum] = cell.NextWithSamePriority;
+				return cell;
+			}
+		}
+		return null;
+	}
 
+	public void Change (HexCell cell, int oldPriority) {
+		HexCell current = list[oldPriority];
+		HexCell next = current.NextWithSamePriority;
+		if (current == cell) {
+			list[oldPriority] = next;
+		}
+		else {
+			while (next != cell) {
+				current = next;
+				next = current.NextWithSamePriority;
+			}
+			current.NextWithSamePriority = cell.NextWithSamePriority;
+		}
+		Enqueue(cell);
+		count -= 1;
+	}
 
-    public void Enqueue(HexCell cell)
-    {
-        count += 1;
-        int priority = cell.SearchPriority;
-        if (priority < minimum)
-        {
-            minimum = priority;
-        }
-
-        while (priority>=list.Count)
-        {
-            list.Add(null);
-        }
-
-        cell.NextWithSamePriority = list[priority];
-        list[priority] = cell;
-        Enqueue(cell);
-    }
-
-    public HexCell Dequeue()
-    {
-        count -= 1;
-        for (;minimum < list.Count; minimum++)
-        {
-            HexCell cell = list[minimum];
-            if (cell != null)
-            {
-                list[minimum] = cell.NextWithSamePriority;
-                return cell;
-            }
-
-        }
-
-        return null;
-    }
-
-    public void Change(HexCell cell,int oldPriority)
-    {
-        HexCell current = list[oldPriority];
-        HexCell next = current.NextWithSamePriority;
-
-        if (current == cell)
-        {
-            list[oldPriority] = next;
-        }
-        else
-        {
-            while (next != cell)
-            {
-                current = next;
-                next = current.NextWithSamePriority;
-            }
-            current.NextWithSamePriority = cell.NextWithSamePriority;
-        }
-    }
-
-    public void Clear()
-    {
-        list.Clear();
-        count = 0;
-        minimum = int.MaxValue;
-    }
+	public void Clear () {
+		list.Clear();
+		count = 0;
+		minimum = int.MaxValue;
+	}
 }
